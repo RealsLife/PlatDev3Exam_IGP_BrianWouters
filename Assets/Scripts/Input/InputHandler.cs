7d54de7351862a526fab615a0f3475066104d849
BrianWouters <brian.wouters@hotmail.com>
2020-01-06 09:16:35 +0100

    InputHandler + InterfaceCommands
    Input handler can receive input from the input manager and uses interfaces to create commands.

--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -3,47 +3,39 @@ using UnityInput = UnityEngine.Input;
 
 namespace Input
 {
-    public class InputHandler : MonoBehaviour
+    public class InputHandler
     {
         public IImpulseCommand ACommand;
         public IImpulseCommand BCommand;
         public IImpulseCommand RightBumper;
         public IImpulseCommand LeftBumper;
         public IVerticalDirectionCommand DPad_Horizontal;
-
+        public IVerticalDirectionCommand DPad_Vertical;
         public void Update()
         {
 
             if (UnityInput.GetButtonDown("A"))
             {
-                Debug.Log("test A");
-                //ACommand.Execute();
+                ACommand.Execute();
             }
 
             if (UnityInput.GetButtonDown("B"))
             {
-                Debug.Log("test B");
+                BCommand.Execute();
             }
 
             if (UnityInput.GetButtonDown("RightBumper"))
             {
-                Debug.Log("test right");
-                //RightBumper.Execute();
+                RightBumper.Execute();
             }
 
             if (UnityInput.GetButtonDown("LeftBumper"))
             {
-                Debug.Log("test left");
-                //RightBumper.Execute();
+                RightBumper.Execute();
             }
 
-            //if()
-
-
-            //float D-Pad
-            //Vector2 directionRight = new Vector2(UnityInput.GetAxis("D-Pad_Horizontal"), UnityInput.GetAxis("D-Pad_Vertical"));
-
-            //RightJoyStickCommand.Execute(directionRight);
+            DPad_Vertical.Execute(UnityInput.GetAxisRaw("D-Pad_Horizontal"));
+            DPad_Horizontal.Execute(UnityInput.GetAxisRaw("D-Pad_Vertical"));
         }
     }
 }
