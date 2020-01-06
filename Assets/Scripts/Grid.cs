a331f5ead89473cf5a30dd39d8211fcea3232529
BrianWouters <brian.wouters@hotmail.com>
2020-01-06 08:56:59 +0100

    Input Manager
    Make the input manager ready to give input.

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    private const int _columns = 10;
+    private const int _row = 10;
+    
+    //void CreateGrid(int column = 10, int row = 10)
+    //{
+    //    for(int i = 0;i < column
+    //}
+}
+
+//veld size
+//maak veld aan
+//view toon veld en is mono behavior
+//
+
