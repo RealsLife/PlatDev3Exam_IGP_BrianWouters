ea2d758b7f16d3543176a9ee9554db7b7c46c85d
BrianWouters <brian.wouters@hotmail.com>
2020-01-10 15:42:57 +0100

    Generic Grid
    Users can create a grid with generics and can override the defaultvalue of the parameters

---
+++ Assets/Scripts/GameArena/Model/Grid.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid<T>
+{
+    public const int GridColumns = 3;
+    public const int GridRows = 3;
+
+    private T _tile;
+    public Grid(int rows = GridRows, int columns = GridColumns)
+    {
+        CreateGrid(rows, columns);
+    }
+
+    private void CreateGrid(int rows,int columns)
+    {
+        T[,] _grid = new T[rows, columns];
+        for (int row = 0; row < _grid.GetLength(0); row++)
+        {
+            for (int column = 0; column < _grid.GetLength(1); column++)
+            {
+                Debug.Log("Test");
+                _grid[row, column] = _tile;
+            }
+        }
+    }
+
+
+    //public void Place(int row, int column)
+    //{
+    //    if (!(row < BoardDimension))
+    //        throw new ArgumentException($"{nameof(row)} should be between 0 and {BoardDimension} (inclusive), passed value was {row}");
+
+    //    if (!(column < BoardDimension))
+    //        throw new ArgumentException($"{nameof(column)} should be between 0 and {BoardDimension} (inclusive), passed value was {column}");
+
+
+    //    _currentTile = _tiles[row, column];
+    //    _currentTile.Content = CurrentPlayer;
+
+    //    if (CheckWinCondition(CurrentPlayer))
+    //    {
+    //        Winner = CurrentPlayer;
+    //        GameOver?.Invoke(this, EventArgs.Empty);
+    //    }
+
+    //    SwapPlayer();
+    //}
+
+
+
+
+    //private void ResetAllTiles()
+    //{
+    //    for (int row = 0; row < GridRows; row++)
+    //    {
+    //        for (int column = 0; column < GridColumns; column++)
+    //        {
+    //            _grid[row, column].Reset();
+    //        }
+    //    }
+    //}
+}
+
+
+
