1f4c5fd894a78c70a4c99f6714ce0bc22267b09e
BrianWouters <brian.wouters@hotmail.com>
2020-01-12 00:18:45 +0100

    Generic GridView
    Creates a view in untiy and also creates tileview

---
+++ Assets/Scripts/GridSystem/GridView.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TileSystem;
+namespace GridSystem
+{
+    public class GridView<T,U> : MonoBehaviour where U : Component
+    {
+        [SerializeField] U _tilePrefab;
+        private Renderer _tileRenderer;
+        private U _tileView;
+        private void Start()
+        {
+            _tileRenderer = _tilePrefab.GetComponent<Renderer>();
+        }
+
+        public void CreateTileView(T tile)
+        {
+            _tileView = GameObject.Instantiate(_tilePrefab, this.transform);
+        }
+
+        public void AllocateTilePosOnGrid(int row, int column)
+        {
+            Vector3 size = _tileRenderer.bounds.size;
+            _tileView.transform.localPosition = new Vector3(column * size.x, 0, row * size.z);
+        }
+
+
+
+
+            //Vector3 size = _renderer.bounds.size;
+            //GameObject jaja = GameObject.Instantiate(_tilePrefabssss, this.transform);
+            //GameObject.Instantiate<U>(_tilePrefab, this.transform);
+            //Instantiate<U>(_tilePrefab, this.transform);
+            //U test = jaja.GetComponent<U>();
+            //aa.transform.localPosition = new Vector3(tile.Column * size.x, 0, tile.Row * size.z);
+
+            //Vector3 size = _renderer.bounds.size;
+            //TileView aa = GameObject.Instantiate(_tilePrefab, this.transform);
+            //aa.transform.localPosition = new Vector3(tile.Column * size.x, 0, tile.Row * size.z);
+        
+    }
+}
+
