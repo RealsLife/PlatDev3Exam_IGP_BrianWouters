5052c211d4947612dd383a2ee2981a52d6a5edfd
BrianWouters <brian.wouters@hotmail.com>
2020-01-15 19:27:15 +0100

    Condtions static class
    conditions can be made and given to units

---
+++ Assets/Scripts/UnitSystem/BuildConditions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitSystem
+{
+    public static class BuildConditions
+    {
+        public static bool DependentOnUnits(int a, int b)
+        {
+            return a > 0 && b > 0;
+        }
+    }
+}
---
+++ Assets/Scripts/UnitSystem/Units.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.Collections;
+
+
+namespace UnitSystem
+{
+    class Units
+    {
+        public Dictionary<int, IUnit> UnitsCatalogue => _units;
+        Dictionary<int, IUnit> _units = new Dictionary<int, IUnit>();
+        void CreateUnits()
+        {
+            _units.Add(1, new KingUnit());
+            _units.Add(1, new QueenUnit());
+            _units.Add(3, new PawnUnit());
+        }
+      
+     
+    }
+}
